540c9ac2fe6278fc89818df2feabea89b571e481
techme@live.cn <techme@live.cn>
2016-08-23 14:50:36 +0800

    修改了几处，更好用

--- megabloggenerator/WebSite/Scripts/youlunhangxian/list.js
+++ megabloggenerator/WebSite/Scripts/youlunhangxian/list.js
@@ -81,12 +81,10 @@ function slideBarFilter() {
         var ov = o.attr('data-v');
         var op = o.parents('.sub-pop');
         var ot = op.siblings('.item-cont').find('.item-bd');
-
         if (o.html().length > 10) {
             var tempDispStr = o.html().substring(o.html().length-10, o.html().length);
             ot.text(tempDispStr);
         } else { ot.text(o.html()); }
-
         ot.attr('data-v', o.attr('data-v'));
         op.hide();
 
@@ -475,7 +473,6 @@ function changeUrl(url) {
 function pageSelectionChange() {
     if (window.history.replaceState == undefined) { console.log("not supported!"); changeUrl(generateUrl()); }
     else { unitFilter(); window.history.replaceState(null, document.title, generateUrl()) }
-    //changeUrl(generateUrl());
 } 
 function dispShip() {
     var name = $("#j_sideSizer .list-item-3 .item-bd.fr").text();
