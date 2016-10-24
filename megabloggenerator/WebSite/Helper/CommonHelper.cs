65c10104c421b20db5dc806426b4e93ce07979b5
megatontech <techme@live.cn>
2016-10-24 13:22:27 +0800

    cherry pick + 生成部分代码

---
+++ megabloggenerator/WebSite/Helper/CommonHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite.Helper
+{
+    public class CommonHelper
+    {
+        /// <summary>
+        /// 生成静态文件
+        /// </summary>
+        /// <param name="storagePath">存储路径</param>
+        /// <param name="fileName">存储文件名</param>
+        /// <param name="pageName">页面名称</param>
+        /// <param name="content">数据model</param>
+        /// <param name="currentContext">当前controller</param>
+        /// <returns></returns>
+        public bool GeneratePage(string storagePath,string fileName,string pageName,object content,ControllerContext currentContext) 
+        {
+            bool success = false;
+            if (!Directory.Exists(Path.GetDirectoryName(storagePath))) { Directory.CreateDirectory(Path.GetDirectoryName(storagePath)); }
+            if (File.Exists(Path.GetDirectoryName(storagePath+fileName))) { File.Delete(Path.GetDirectoryName(storagePath + fileName)); }
+            ViewEngineResult result = ViewEngines.Engines.FindView(currentContext, pageName, "");
+            currentContext.Controller.ViewData.Model = content;
+            //currentContext.Controller.TempData.Add("IsStatic",true);
+            if (result.View != null) 
+            {
+                //生成
+                using (StringWriter writer = new StringWriter()) 
+                {
+                    string resultHtmlStr = string.Empty;
+                    ViewContext pageContext = new ViewContext(currentContext,result.View,currentContext.Controller.ViewData,currentContext.Controller.TempData,writer);
+                    result.View.Render(pageContext,writer);
+                    resultHtmlStr = writer.ToString();
+                    File.WriteAllText(storagePath + fileName, resultHtmlStr);
+                }
+                success = true; 
+            }
+            else { success = false; }
+            return success;
+        }
+    }
+}
---
+++ megabloggenerator/WebSite/Models/PagePreview.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.Models
+{
+    public class PagePreview
+    {
+        public POST post { get; set; }
+        public Templete templete { get; set; }
+    }
+}
